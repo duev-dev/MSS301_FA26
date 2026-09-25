d0cb76e08af2a2fd380c7acffdce4b1060f0bd95
devdwillight <ducle0554@gmail.com>
2026-09-25 16:35:00 +0700

    feat: OrderService & flyway config

---
+++ Slot5/order-service/src/main/java/com/fudn/orderservice/dto/OrderRequest.java
@@ -0,0 +1,13 @@
+package com.fudn.orderservice.dto;
+
+import jakarta.validation.constraints.NotNull;
+
+import java.math.BigDecimal;
+
+public record OrderRequest(Long id,
+                           @NotNull(message = "skuCode must not be null")
+                           String skuCode,
+                           BigDecimal price,
+                           Integer quantity) {
+
+}
---
+++ Slot5/order-service/src/main/java/com/fudn/orderservice/model/Order.java
@@ -0,0 +1,24 @@
+package com.fudn.orderservice.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "t_orders")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String orderNumber;
+    private String skuCode;
+    private BigDecimal price;
+    private Integer quantity;
+}
